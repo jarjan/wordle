8e4a644362faa1cbb94caa73918d95d64c57858d
Zharzhan Kulmyrza <hey@jarjan.xyz>
2022-01-22 21:18:58 +0100

    Keyboard buttons improvement

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,19 @@
-import Tile from "./Tile";
+import cls from "classnames";
+
+const Tile = ({ value, isAnswered, isCorrect, isInPosition }) => {
+  return (
+    <span
+      class={cls("board__tile", {
+        empty: value === "",
+        answered: isAnswered,
+        correct: isCorrect,
+        inPosition: isInPosition,
+      })}
+    >
+      {value}
+    </span>
+  );
+};
 
 const Board = ({ answers, guess, chance, todayWord }) => {
   return (
